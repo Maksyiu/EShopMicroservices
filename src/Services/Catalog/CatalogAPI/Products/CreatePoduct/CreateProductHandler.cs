178d13821a08f71302e74308597ba549f3f2dfee
Lukasz <l@o2.pl>
2024-04-26 00:56:56 +0200

    Commit after chapter 54

---
+++ src/Services/Catalog/CatalogAPI/Products/CreatePoduct/CreateProductHandler.cs
@@ -0,0 +1,39 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+using MediatR;
+using System.ComponentModel;
+
+namespace CatalogAPI.Products.CreatePoduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, 
+        string ImageFile, decimal Price) 
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Business logic to create a product
+            // create product entity
+            // save to database
+            // return createproduct result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // TODO: save to database - pozniej to zrobimy
+
+            // return result
+
+            return new CreateProductResult(Guid.NewGuid());            
+        }
+    }
+}
